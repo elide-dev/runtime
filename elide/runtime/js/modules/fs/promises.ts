311b9dac9b6ae1740071abd6f6a0ceaeb2c15214
Sam Gammon <sam@elide.ventures>
2024-06-16 22:56:30 -0700

    fix: deferred init of module intrinsics
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- elide/runtime/js/modules/fs/promises.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Intrinsic: Filesystem (Promises).
- *
- * Provides a shim which offers a `fs/promises` module implementation that is compatible with Node.js-style imports.
- */
-
-export default {};
-
