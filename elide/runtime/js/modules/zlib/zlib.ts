1f6e57c2d6cbfc7a66002bb0f7b7086d141a5116
Sam Gammon <sam@elide.dev>
2025-02-04 22:31:53 -0800

    feat(node): implement `zlib`
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- elide/runtime/js/modules/zlib/zlib.ts
+++ elide/runtime/js/modules/zlib/zlib.ts
@@ -17,4 +17,95 @@
  * Provides access to the Zlib compression and decompression library.
  */
 
-export {};
+const { node_zlib } = primordials;
+if (!node_zlib) {
+  throw new Error(`The 'zlib' module failed to load the intrinsic API.`);
+}
+
+function intrinsic(): any {
+  return node_zlib;
+}
+
+/**
+ * Provides constants defined by the `zlib` module.
+ */
+export const constants = intrinsic().constants;
+
+/**
+ * Calculates a CRC32 checksum for the given data.
+ *
+ * @param data The data to checksum
+ * @param value The initial value for the checksum (optional)
+ * @returns The CRC32 checksum for the given data
+ */
+export function crc32(data: string | Buffer | DataView | any, value?: number): number {
+    return intrinsic().crc32(data, value);
+}
+
+export type Deflate = any;
+
+export type Inflate = any;
+
+/**
+ * Creates a writable stream which compresses data using the DEFLATE algorithm.
+ *
+ * @param options Zlib options to apply to this stream. Optional.
+ * @returns Deflation stream
+ */
+export function createDeflate(options?: any): Deflate {
+    return intrinsic().createDeflate(options);
+}
+
+/**
+ * Creates a readable stream which decompresses data using the DEFLATE algorithm.
+ *
+ * @param options Zlib options to apply to this stream. Optional.
+ * @returns Inflation stream
+ */
+export function createInflate(options?: any): Inflate {
+    return intrinsic().createInflate(options);
+}
+
+/**
+ * Synchronously compresses the given data using the DEFLATE algorithm.
+ *
+ * @param data The data to compress
+ * @param options Zlib options to apply to this compression. Optional.
+ * @returns The compressed data
+ */
+export function deflateSync(data: string | Buffer | DataView | any, options?: any): Buffer {
+    return intrinsic().deflateSync(data, options);
+}
+
+/**
+ * Synchronously decompresses the given data using the DEFLATE algorithm.
+ *
+ * @param data The data to decompress
+ * @param options Zlib options to apply to this decompression. Optional.
+ * @returns The decompressed data
+ */
+export function inflateSync(data: string | Buffer | DataView | any, options?: any): Buffer {
+    return intrinsic().inflateSync(data, options);
+}
+
+/**
+ * Synchronously compresses the given data using the gzip algorithm.
+ *
+ * @param data The data to compress
+ * @param options Zlib options to apply to this compression. Optional.
+ * @returns The compressed data
+ */
+export function gzipSync(data: string | Buffer | DataView | any, options?: any): Buffer {
+    return intrinsic().gzipSync(data, options);
+}
+
+/**
+ * Synchronously decompresses the given data using the gzip algorithm.
+ *
+ * @param data The data to decompress
+ * @param options Zlib options to apply to this decompression. Optional.
+ * @returns The decompressed data
+ */
+export function gunzipSync(data: string | Buffer | DataView | any, options?: any): Buffer {
+    return intrinsic().gunzipSync(data, options);
+}
