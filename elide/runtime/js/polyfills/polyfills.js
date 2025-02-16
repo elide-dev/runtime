5f210749fffc96a5b3d1e42e4a6992ddecd8204d
Sam Gammon <sam@elide.dev>
2025-02-15 16:01:57 -0800

    chore: drop polyfills for abort types
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- elide/runtime/js/polyfills/polyfills.js
+++ elide/runtime/js/polyfills/polyfills.js
@@ -11,6 +11,6 @@
  * License for the specific language governing permissions and limitations under the License.
  */
 
-import "./abort-controller.js";
 import "./once.js";
 import "./readable-stream.mjs";
+
